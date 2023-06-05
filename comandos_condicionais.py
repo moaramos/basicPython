507e105c616ef6ed50de9f1cf75e1dbccb66ed7f
Moacir Ramos <moaramos@live.com>
2023-06-05 17:14:25 -0300

    Add files via upload

---
+++ comandos_condicionais.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#condicionais
+
+a = 2; b = 3; c = 4
+
+if a > b:
+	print("'a' é maior que 'b'")
+else:
+	print("'b' é maior que 'a'")
+
+print("--------")
+#comando elif (cai no primeiro que der "True")
+
+if a == b:
+	print("numeros iguais")
+elif a < b:
+	print("'a' menor que 'b'")
+else:
+	print("numeros sao diferentes")
